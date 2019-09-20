21c0f8366ca80a21a2a8b146443a5bdd3b4252af
Hungming Chen <nuccachen@google.com>
2019-09-20 18:38:47 +0800

    resolv_gold_test: Use proto text file to store test data
    Wrap DNS query API arguments and its corresponding DNS packets
    from real life network into proto text file. The proto text
    file is used to be gold test data.
    
    Test: cd packages/modules/DnsResolver && atest
    Change-Id: If3e73fee093c6e2228950c61ec11050f90f9b770

--- tests/resolv_gold_test.cpp
+++ tests/resolv_gold_test.cpp
@@ -17,11 +17,14 @@
 
 #define LOG_TAG "resolv_gold_test"
 
+#include <android-base/file.h>
 #include <gmock/gmock-matchers.h>
+#include <google/protobuf/text_format.h>
 #include <gtest/gtest.h>
 
 #include "dns_responder/dns_responder.h"
 #include "getaddrinfo.h"
+#include "golddata.pb.h"
 #include "resolv_cache.h"
 #include "resolv_test_utils.h"
 
@@ -68,6 +71,7 @@ static const std::vector<uint8_t> kHelloExampleComResponseV4 = {
         0x01, 0x02, 0x03, 0x04  /* Address: 1.2.3.4 */
 };
 
+// Fixture test class definition.
 class TestBase : public ::testing::Test {
   protected:
     void SetUp() override {
@@ -86,6 +90,8 @@ class TestBase : public ::testing::Test {
         return resolv_set_nameservers(TEST_NETID, servers, domains, kParams);
     }
 
+    const std::string kTestPath = android::base::GetExecutableDirectory();
+    const std::string kTestDataPath = kTestPath + "/testdata/";
     static constexpr res_params kParams = {
             .sample_validity = 300,
             .success_threshold = 25,
@@ -94,7 +100,6 @@ class TestBase : public ::testing::Test {
             .base_timeout_msec = 1000,
             .retry_count = 2,
     };
-
     static constexpr android_net_context kNetcontext = {
             .app_netid = TEST_NETID,
             .app_mark = MARK_UNSET,
@@ -103,9 +108,23 @@ class TestBase : public ::testing::Test {
             .uid = NET_CONTEXT_INVALID_UID,
     };
 };
-
 class ResolvGetAddrInfo : public TestBase {};
 
+// Parameterized test class definition.
+class ResolvGoldTest : public TestBase, public ::testing::WithParamInterface<std::string> {};
+
+// GetAddrInfo tests.
+INSTANTIATE_TEST_SUITE_P(GetAddrInfo, ResolvGoldTest,
+                         ::testing::Values(std::string("getaddrinfo.topsite.google.pbtxt")),
+                         [](const ::testing::TestParamInfo<std::string>& info) {
+                             std::string name = info.param;
+                             std::replace_if(
+                                     std::begin(name), std::end(name),
+                                     [](char ch) { return !std::isalnum(ch); }, '_');
+                             return name;
+                         });
+
+// Fixture tests.
 TEST_F(ResolvGetAddrInfo, RemovePacketMapping) {
     test::DNSResponder dns(test::DNSResponder::MappingType::BINARY_PACKET);
     ASSERT_TRUE(dns.startServer());
@@ -181,6 +200,62 @@ TEST_F(ResolvGetAddrInfo, ReplacePacketMapping) {
     ASSERT_NE(nullptr, result);
     ASSERT_EQ(0, rv);
     EXPECT_EQ("5.6.7.8", ToString(result));
+}
+
+// Parameterized tests.
+TEST_P(ResolvGoldTest, GoldData) {
+    const auto& testFile = GetParam();
+
+    // Convert the testing configuration from .pbtxt file to proto.
+    std::string file_content;
+    ASSERT_TRUE(android::base::ReadFileToString(kTestDataPath + testFile, &file_content))
+            << strerror(errno);
+    android::net::GoldTest goldtest;
+    ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(file_content, &goldtest));
+    ASSERT_EQ(android::net::CallType::CALL_GETADDRINFO, goldtest.config().call());
+    ASSERT_TRUE(goldtest.config().has_addrinfo());
+
+    test::DNSResponder dns(test::DNSResponder::MappingType::BINARY_PACKET);
+    ASSERT_TRUE(dns.startServer());
+    ASSERT_EQ(0, SetResolvers());
+
+    // Register packet mapping (query, response) from proto.
+    for (const auto& m : goldtest.packet_mapping()) {
+        // Convert string to bytes because .proto type "bytes" is "string" type in C++.
+        // See also the section "Scalar Value Types" in "Language Guide (proto3)".
+        dns.addMappingBinaryPacket(std::vector<uint8_t>(m.query().begin(), m.query().end()),
+                                   std::vector<uint8_t>(m.response().begin(), m.response().end()));
+    }
+
+    addrinfo* res = nullptr;
+    const auto& args = goldtest.config().addrinfo();
+    const addrinfo hints = {
+            .ai_family = args.family(),
+            .ai_socktype = args.socktype(),
+            .ai_protocol = args.protocol(),
+            // Clear the flag AI_ADDRCONFIG to avoid flaky test because AI_ADDRCONFIG looks at
+            // whether connectivity is available. It makes that the resolver may send only A
+            // or AAAA DNS query per connectivity even AF_UNSPEC has been assigned. See also
+            // have_ipv6() and have_ipv4() in packages/modules/DnsResolver/getaddrinfo.cpp.
+            // TODO: Consider keeping the configuration flag AI_ADDRCONFIG once the unit
+            // test can treat the IPv4 and IPv6 connectivity.
+            .ai_flags = args.ai_flags() & ~AI_ADDRCONFIG,
+    };
+    NetworkDnsEventReported event;
+    int rv = resolv_getaddrinfo(args.host().c_str(), nullptr, &hints, &kNetcontext, &res, &event);
+    ScopedAddrinfo result(res);
+    ASSERT_EQ(goldtest.result().return_code(), rv);
+
+    if (goldtest.result().return_code() != GT_EAI_NO_ERROR) {
+        ASSERT_EQ(nullptr, result);
+    } else {
+        ASSERT_NE(nullptr, result);
+        const auto& addresses = goldtest.result().addresses();
+        EXPECT_THAT(ToStrings(result),
+                    ::testing::UnorderedElementsAreArray(
+                            std::vector<std::string>(addresses.begin(), addresses.end())));
+    }
+    EXPECT_EQ((hints.ai_family == AF_UNSPEC) ? 2U : 1U, GetNumQueries(dns, args.host().c_str()));
 }
 
 }  // end of namespace net
