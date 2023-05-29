768d4aa22f3b9224e544545ad1e24d8d4c8878d2
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-29 07:54:54 +0300

    extend connectivity checks setting to DNS-over-HTTPS probe
    13: bfa41cfc
    
    Change-Id: Ied557d16388c73cb8ec39798aadb284cd1ee3c8c

--- doh/encoding.rs
+++ doh/encoding.rs
@@ -18,8 +18,10 @@
 
 use anyhow::{anyhow, Context, Result};
 use base64::{prelude::BASE64_URL_SAFE_NO_PAD, Engine};
+use log::debug;
 use quiche::h3;
 use ring::rand::SecureRandom;
+use rustutils::system_properties;
 use url::Url;
 
 pub type DnsRequest = Vec<quiche::h3::Header>;
@@ -31,6 +33,12 @@ const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                          abcdefghijklmnopqrstuvwxyz\
                          0123456789";
 
+// keep in sync with android.ext.settings.ConnChecksSetting
+pub const CONN_CHECKS_SETTING_GRAPHENEOS: i32 = 0;
+pub const CONN_CHECKS_SETTING_STANDARD: i32 = 1;
+pub const CONN_CHECKS_SETTING_DISABLED: i32 = 2;
+pub const CONN_CHECKS_SETTING_DEFAULT: i32 = CONN_CHECKS_SETTING_GRAPHENEOS;
+
 /// Produces a DNS query with randomized query ID and random 6-byte charset-legal prefix to produce
 /// a request for a domain of the form:
 /// ??????-dnsohttps-ds.metric.gstatic.com
@@ -39,7 +47,7 @@ pub fn probe_query() -> Result<String> {
     let mut rnd = [0; 8];
     ring::rand::SystemRandom::new().fill(&mut rnd).context("failed to generate probe rnd")?;
     let c = |byte| CHARSET[(byte as usize) % CHARSET.len()];
-    let query = vec![
+    let mut query = vec![
         rnd[6], rnd[7],  // [0-1]   query ID
         1,      0,       // [2-3]   flags; query[2] = 1 for recursion desired (RD).
         0,      1,       // [4-5]   QDCOUNT (number of queries)
@@ -48,13 +56,52 @@ pub fn probe_query() -> Result<String> {
         0,      0,       // [10-11] ARCOUNT (number of additional records)
         19,     c(rnd[0]), c(rnd[1]), c(rnd[2]), c(rnd[3]), c(rnd[4]), c(rnd[5]), b'-', b'd', b'n',
         b's',   b'o',      b'h',      b't',      b't',      b'p',      b's',      b'-', b'd', b's',
-        6,      b'm',      b'e',      b't',      b'r',      b'i',      b'c',      7,    b'g', b's',
-        b't',   b'a',      b't',      b'i',      b'c',      3,         b'c',      b'o', b'm',
+    ];
+
+    let conn_check_setting: i32 = system_properties::read("persist.sys.connectivity_checks")
+        .unwrap_or_else(|_| Some("".to_string()))
+        .as_deref()
+        .unwrap_or("")
+        .parse::<i32>()
+        .map_or_else(|_| CONN_CHECKS_SETTING_DEFAULT, |v| match v {
+            CONN_CHECKS_SETTING_GRAPHENEOS
+            | CONN_CHECKS_SETTING_STANDARD => v,
+            CONN_CHECKS_SETTING_DISABLED => {
+                // skipping the check would break detection of broken DoH
+                debug!("probe_query: CONN_CHECKS_SETTING_DISABLED, falling back to default hostname");
+                CONN_CHECKS_SETTING_DEFAULT
+            },
+            _ => CONN_CHECKS_SETTING_DEFAULT,
+        });
+
+    if conn_check_setting == CONN_CHECKS_SETTING_STANDARD {
+        debug!("probe_query: using metric.gstatic.com");
+        append_qname_label("metric", &mut query);
+        append_qname_label("gstatic", &mut query);
+        append_qname_label("com", &mut query);
+    } else {
+        debug!("probe_query: using dnscheck.grapheneos.org");
+        append_qname_label("dnscheck", &mut query);
+        append_qname_label("grapheneos", &mut query);
+        append_qname_label("org", &mut query);
+    }
+
+    let mut end = vec![
         0,                  // null terminator of FQDN (root TLD)
         0,      NS_T_AAAA,  // QTYPE
         0,      NS_C_IN     // QCLASS
     ];
+    query.append(&mut end);
+
     Ok(BASE64_URL_SAFE_NO_PAD.encode(query))
+}
+
+fn append_qname_label(label: &str, dst: &mut Vec<u8>) {
+    let len = label.len();
+    // 6-bit positive number
+    assert!(len > 0 && len <= 0b11_1111);
+    dst.push(len as u8);
+    dst.extend_from_slice(label.as_bytes());
 }
 
 /// Takes in a base64-encoded copy of a traditional DNS request and a
