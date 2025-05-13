a3df5649ea5267edd3d0dcc1dc32a81223907cbf
Ningyuan Wang <ningyuan@google.com>
2025-05-13 15:12:06 +0900

    dns_proxy: downstream UDP socket configuration
    Flag: EXEMPT implementation of unused IDnsResolver AIDL
    Bug: 379992903
    Test: m; cargo test
    Change-Id: Iea98408840fe6dfa3a928c6e524acfa0668116f0

--- dns_proxy/server/driver.rs
+++ dns_proxy/server/driver.rs
@@ -16,9 +16,24 @@
 
 //! Provides a backing task to implement a Server
 
+use std::collections::hash_map::Entry as HashMapEntry;
 use std::collections::HashMap;
+use std::net::IpAddr;
+use std::net::Ipv6Addr;
+use std::net::SocketAddr;
+use std::net::UdpSocket as SyncUdpSocket;
+use std::os::fd::AsFd;
+use std::os::fd::AsRawFd;
+use std::rc::Rc;
 
 use log::info;
+use nix::libc::c_int;
+use nix::libc::setsockopt;
+use socket2::Domain;
+use socket2::Protocol;
+use socket2::Socket;
+use socket2::Type;
+use tokio::net::UdpSocket;
 use tokio::sync::mpsc;
 
 use super::Command;
@@ -26,15 +41,21 @@ use super::DownstreamIndexPort;
 use super::Result;
 use super::UpstreamParam;
 
+// TODO(b/409455084): workaround while waiting for upstream changes.
+const SO_BINDTOIFINDEX: c_int = 62;
+
+#[derive(Debug)]
 pub(super) struct Driver {
     command_rx: mpsc::Receiver<Command>,
     /// Map of DownstreamIndexPort pair to the upstream parameters.
     upstream_map: HashMap<DownstreamIndexPort, UpstreamParam>,
+    /// Map of DownstreamIndexPort pair to the UDP socket it is listening to.
+    downstream_socket_map: HashMap<DownstreamIndexPort, DownstreamUdpSocket>,
 }
 
 impl Driver {
     pub fn new(command_rx: mpsc::Receiver<Command>) -> Self {
-        Self { command_rx, upstream_map: HashMap::new() }
+        Self { command_rx, upstream_map: HashMap::new(), downstream_socket_map: HashMap::new() }
     }
 
     pub async fn drive(mut self) -> Option<()> {
@@ -74,11 +95,56 @@ impl Driver {
         upstream_param: UpstreamParam,
     ) -> Result<()> {
         self.upstream_map.insert(index_port, upstream_param);
+        if let HashMapEntry::Vacant(vacant_entry) = self.downstream_socket_map.entry(index_port) {
+            let sync_socket = Driver::build_udp_socket(&index_port)?;
+            let socket = DownstreamUdpSocket::new(Rc::new(UdpSocket::from_std(sync_socket)?));
+            vacant_entry.insert(socket);
+        }
+        Ok(())
+    }
+
+    fn build_udp_socket(index_port: &DownstreamIndexPort) -> Result<SyncUdpSocket> {
+        let socket = Socket::new(Domain::IPV6, Type::DGRAM, Some(Protocol::UDP))?;
+        Driver::set_downstream_sockopts(&socket, index_port.if_index)?;
+        socket
+            .bind(&SocketAddr::new(IpAddr::V6(Ipv6Addr::from_bits(0)), index_port.port).into())?;
+        Ok(socket.into())
+    }
+
+    fn set_downstream_sockopts(socket: &Socket, if_index: u32) -> Result<()> {
+        socket.set_nonblocking(true)?;
+        if if_index > 0 {
+            // TODO(409455084): workaround while waiting for upstream changes.
+            // Safety: setting if_index is safe since we own the FD and if_index is fixed length.
+            unsafe {
+                setsockopt(
+                    socket.as_fd().as_raw_fd(),
+                    nix::libc::SOL_SOCKET,
+                    SO_BINDTOIFINDEX,
+                    &if_index as *const _ as *const nix::libc::c_void,
+                    std::mem::size_of::<c_int>() as nix::libc::socklen_t,
+                );
+            }
+        }
         Ok(())
     }
 
     fn handle_stop_dns_proxy_cmd(&mut self, index_port: &DownstreamIndexPort) -> Result<()> {
         self.upstream_map.remove(index_port);
         Ok(())
+    }
+}
+
+/// Downstream UDP socket
+#[derive(Debug)]
+struct DownstreamUdpSocket {
+    /// Reference-counted socket
+    socket: Rc<UdpSocket>,
+}
+
+impl DownstreamUdpSocket {
+    /// Constructor
+    fn new(socket: Rc<UdpSocket>) -> Self {
+        Self { socket }
     }
 }
