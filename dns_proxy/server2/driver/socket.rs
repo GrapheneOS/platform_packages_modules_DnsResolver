8842431e4e1e329c1a069bf95dc1f4930ec0da84
Patrick Rohr <prohr@google.com>
2025-08-08 12:21:49 -0700

    Implement UdpServerSocket
    tokio::net::UdpSocket is difficult to use for our purposes, so
    UdpServerSocket wraps the tokio::net::UdpSocket and implements
    recv_from_with_ifindex on top of it (using the recvmsg implementation in
    the extension trait). This is not the final version of it yet. In the
    final version, I want to be able to receive an arbitrarily sized buffer,
    which will be achieved by replicating udp_recv in socket/driver.rs.
    
    Test: TH
    Flag: EXEMPT mainline
    Change-Id: I85a2988e3284c20472cef0b8e5b8e3ede7590c00

--- dns_proxy/server2/driver/socket.rs
+++ dns_proxy/server2/driver/socket.rs
@@ -14,13 +14,78 @@
  * limitations under the License.
  */
 
+use anyhow::ensure;
+use anyhow::Result;
+use nix::cmsg_space;
+use nix::libc::in6_pktinfo;
 use nix::sys::socket::recv;
 use nix::sys::socket::recvmsg;
+use nix::sys::socket::setsockopt;
+use nix::sys::socket::sockopt::Ipv6RecvPacketInfo;
+use nix::sys::socket::ControlMessageOwned;
 use nix::sys::socket::MsgFlags;
 use nix::sys::socket::RecvMsg;
 use nix::sys::socket::SockaddrLike;
 use std::io::IoSliceMut;
+use std::net::Ipv6Addr;
+use std::net::SocketAddrV6;
 use tokio::net::UdpSocket;
+
+/// A UDP socket that can receive packets with their destination interface index.
+pub struct UdpServerSocket {
+    socket: UdpSocket,
+}
+
+impl UdpServerSocket {
+    /// Creates a new UdpServerSocket from a std::net::UdpSocket.
+    pub fn new(socket: std::net::UdpSocket) -> Result<Self> {
+        // UdpServerSocket must be a dual-stack socket.
+        ensure!(socket.local_addr()?.ip() == Ipv6Addr::UNSPECIFIED, "Socket must be dual-stack");
+
+        socket.set_nonblocking(true)?;
+        let socket = UdpSocket::from_std(socket)?;
+        setsockopt(&socket, Ipv6RecvPacketInfo, &true)?;
+        Ok(Self { socket })
+    }
+
+    /// Receives a single datagram from the socket. On success, returns the number
+    /// of bytes read, the sender's address, and the interface index. Must be used in combination
+    /// with readable().
+    #[allow(clippy::unnecessary_cast)]
+    fn try_recv_from_with_ifindex(&self, buf: &mut [u8]) -> Result<(usize, SocketAddrV6, u32)> {
+        let mut cmsg_buf = cmsg_space!(in6_pktinfo);
+        let iov = &mut [IoSliceMut::new(buf)];
+        let msg = self.socket.try_recvmsg::<nix::sys::socket::SockaddrIn6>(
+            iov,
+            Some(&mut cmsg_buf),
+            MsgFlags::empty(),
+        )?;
+
+        // Note: different versions of Rust libc define ipi6_ifindex as u32 or i32. Force the cast
+        // to u32 for compatibility.
+        let ifindex = msg
+            .cmsgs()?
+            .find_map(|cmsg| {
+                if let ControlMessageOwned::Ipv6PacketInfo(packet_info) = cmsg {
+                    Some(packet_info.ipi6_ifindex)
+                } else {
+                    None
+                }
+            })
+            .unwrap_or(0) as u32;
+
+        let addr = msg.address.map(SocketAddrV6::from).unwrap();
+        Ok((msg.bytes, addr, ifindex))
+    }
+
+    pub async fn recv_from_with_ifindex(
+        &self,
+        buf: &mut [u8],
+    ) -> Result<(usize, SocketAddrV6, u32)> {
+        self.socket.readable().await?;
+        self.try_recv_from_with_ifindex(buf)
+    }
+}
 
 /// UdpSocket extension trait for implementing UdpSocket functionality that is missing in
 /// tokio::net::UdpSocket.
@@ -66,5 +131,45 @@ impl UdpSocketExt for UdpSocket {
             use std::os::fd::AsRawFd as _;
             recvmsg(self.as_raw_fd(), iov, cmsg_buf, flags).map_err(std::io::Error::from)
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::packet::tests::TEST_VALID_DNS_QUERY;
+    use std::net::Ipv4Addr;
+    use std::net::SocketAddr;
+
+    #[tokio::test]
+    async fn test_udp_server_socket_new() {
+        let std_socket = std::net::UdpSocket::bind("[::]:0").unwrap();
+        let server_socket = UdpServerSocket::new(std_socket);
+        assert!(server_socket.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_udp_server_socket_recv_from_with_ifindex() {
+        let std_socket = std::net::UdpSocket::bind("[::]:0").unwrap();
+        let server_port = std_socket.local_addr().unwrap().port();
+        let server_socket = UdpServerSocket::new(std_socket).unwrap();
+
+        let client_socket = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+        let client_addr = client_socket.local_addr().unwrap();
+        let server_addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), server_port);
+
+        client_socket.connect(server_addr).await.unwrap();
+        client_socket.send(&TEST_VALID_DNS_QUERY).await.unwrap();
+
+        let mut buf = [0u8; 1024];
+        let (len, addr, ifindex) = server_socket.recv_from_with_ifindex(&mut buf).await.unwrap();
+
+        assert_eq!(len, TEST_VALID_DNS_QUERY.len());
+        assert_eq!(&buf[..len], &TEST_VALID_DNS_QUERY);
+        // Must convert addresses to canonical representation, so the IPv6-mapped IPv4 addresses
+        // compare correctly.
+        assert_eq!(addr.ip().to_canonical(), client_addr.ip().to_canonical());
+        assert_eq!(addr.port(), client_addr.port());
+        assert_eq!(ifindex, 1 /* loopback */);
     }
 }
